44c127ee9fab9006bc3096c04cb29003738b6e5d
rehtsezzz <fannyuwu901@gmail.com>
2026-04-16 12:22:31 -0400

    Creación de las migraciones
    Creación de las migraciones de base de datos

--- aUroNMerCH/Program.cs
+++ aUroNMerCH/Program.cs
@@ -1,23 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder =using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
